f44bbcdadaf545f29c236a2a6886526c9e3f3170
Elizeu Vailant <83087208+movingGreen@users.noreply.github.com>
2022-11-25 13:58:07 -0400

    Add files via upload

--- Atividade_Teste_LexisNexis.js
+++ Atividade_Teste_LexisNexis.js
@@ -1,7 +1,8 @@
-const regexEspacoEmBranco = /\s+/g;
-const regexCPF = /\d{3}\.\d{3}\.\d{3}-\d{2}/g;
-const regexData = ;
-const regexTelefone = ;
-const regexCNPJ = ;
-const regexEmail = ;
-const regexCartao = ;
+// Declaração dos padrões de regex 
+const REGEX_CPF = /\d{3}\.\d{3}\.\d{3}-\d{2}/g;
+const REGEX_DATA = /\d{2}\/\d{2}\/\d{4}/g;
+const REGEX_TELEFONE = /\(\d{2}\)\d{5}-\d{4}/g;
+const REGEX_CNPJ = /\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}/g;
+const REGEX_EMAIL = /[A-Z][a-z]+@[a-z]+\.[a-z]+/g;
+const REGEX_CARTAO = /(\d{4}-\d{4}-\d{4}-\d{4})|(\d{4} \d{4} \d{4} \d{4})|(\d{4}\.\d{4}\.\d{4}\.\d{4})/g;
+
