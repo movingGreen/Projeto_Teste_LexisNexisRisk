72991817e964cac62163b7d2baece04f486b5395
Elizeu Vailant <83087208+movingGreen@users.noreply.github.com>
2022-11-25 10:14:42 -0400

    Começo testes

---
+++ Atividade_Teste_LexisNexis.js
@@ -0,0 +1,7 @@
+const regexEspacoEmBranco = /\s+/g;
+const regexCPF = /\d{3}\.\d{3}\.\d{3}-\d{2}/g;
+const regexData = ;
+const regexTelefone = ;
+const regexCNPJ = ;
+const regexEmail = ;
+const regexCartao = ;
